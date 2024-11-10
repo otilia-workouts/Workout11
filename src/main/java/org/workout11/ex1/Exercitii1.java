d34463130515056cc0e45b4e6da4c4bfcb01aa98
Otilia Chelmuș <62885587+otiliach@users.noreply.github.com>
2024-11-10 17:02:20 +0200

    I added multiply matrix, intersection, union and difference methods.

--- src/main/java/org/workout11/ex1/Exercitii1.java
+++ src/main/java/org/workout11/ex1/Exercitii1.java
@@ -1,12 +1,12 @@
 package org.workout11.ex1;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class Exercitii1 {
 
     /**
      * TODO:a) Scrieti o metoda ce primeste doua Stringuri si verifica daca unul dintre ele este substring al celuilalt (eg: text si context sau javascript si java)
+     *
      * @param a is String;
      * @param b is String.
      * @return true if 'a' is substring of 'b' or 'b' is substring of 'b'
@@ -19,37 +19,93 @@ public class Exercitii1 {
     /**
      * TODO:b) Scrieti o metoda ce verifica daca un String se potriveste pe patternul unui numar de inmatriculare romanesc (Eg. "B 153 ABC")
      * Folositi ca resurse: https://www.vogella.com/tutorials/JavaRegularExpressions/article.html si https://www.freeformatter.com/java-regex-tester.html#ad-output
+     *
      * @param plateNumber a String text
      * @return true if the plateNumber matches the model of Romanian plate numbers,
-     *  <br> false otherwise.
+     * <br> false otherwise.
      */
     public static boolean isPlateNumber(String plateNumber) {
         return plateNumber.matches("(B\\s[0-9]{2,3}|[A-Z]{2}\\s[0-9]{2})\\s[A-PR-Z]{3}");
     }
 
 
-
     /**
      * //TODO:c) Scrieti o metoda ce elimina elementele duplicate dintr-un array
      */
-    public static ArrayList<?> removeDuplicates(ArrayList<?> arrayList){
-        return new ArrayList<>( new LinkedHashSet<>(arrayList));
+    public static ArrayList<?> removeDuplicates(ArrayList<?> arrayList) {
+        return new ArrayList<>(new LinkedHashSet<>(arrayList));
     }
 
-    /**TODO:d) Scrieti o metoda ce verifica de cate ori se gaseste un anumit element intr-un array
+    /**
+     * TODO:d) Scrieti o metoda ce verifica de cate ori se gaseste un anumit element intr-un array
      */
-    public static int occurences(Object element, ArrayList<?> arrayList){
+    public static int occurences(Object element, ArrayList<?> arrayList) {
         int counter = 0;
-        for(var elementArray: arrayList){
-            if(elementArray.equals(element)){
+        for (var elementArray : arrayList) {
+            if (elementArray.equals(element)) {
                 counter++;
             }
         }
         return counter;
     }
 
-    //TODO:e) Scrieti o metoda ce inmulteste (in sens matematic) doua matrice.
+    /**
+     * TODO:e) Scrieti o metoda ce inmulteste (in sens matematic) doua matrice.
+     */
+    public static Integer[][] multiplyMatrix(Integer[][] matrixA, Integer[][] matrixB) throws Exception {
+        int rowsM = matrixA.length, columnsN = matrixA[0].length, columnsP = matrixB[0].length;
+        Integer[][] matrix = new Integer[rowsM][columnsP];
+        for (Integer[] row : matrix) {
+            Arrays.fill(row, 0);
+        }
 
-    //TODO:f) Creati doua HashSeturi si realizati metode pentru operatiile caracteristice multimilor intersectie, reuniune si diferenta.
+        if (matrixA[0].length != matrixB.length) {
+            throw new Exception("matrixA[0].length must be equal with matrixB.length.");
+        }
+
+        for (int i = 0; i < rowsM; i++) {
+            for (int j = 0; j < columnsP; j++) {
+                for (int k = 0; k < columnsN; k++) {
+                    matrix[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+
+
+        return matrix;
+    }
+
+    /**
+     * TODO:f) Creati doua HashSeturi si realizati metode pentru operatiile caracteristice multimilor intersectie, reuniune si diferenta.
+     */
+    public static Set<?> intersect(Set<?> hashSetA, Set<?> hashSetB) {
+        Set<Object> intersection = new HashSet<>(hashSetA);
+        intersection.retainAll(hashSetB);
+        return intersection;
+    }
+
+    public static Set<?> union(Set<?> hashSetA, Set<?> hashSetB) {
+        Set<Object> setUnion = new HashSet<>(hashSetA);
+        setUnion.addAll(hashSetB);
+        return setUnion;
+    }
+
+    public static Set<?> symmetricDifference(Set<?> hashSetA, Set<?> hashSetB) {
+        Set<Object> diff = new HashSet<>(union(hashSetA, hashSetB));
+        diff.removeAll(intersect(hashSetA, hashSetB));
+        return diff;
+    }
+
+    /**
+     *
+     * @param hashSetA
+     * @param hashSetB
+     * @return hashSetA-hashSetB
+     */
+    public static Set<?> difference(Set<?> hashSetA, Set<?> hashSetB) {
+        Set<Object> diff = new HashSet<>(hashSetA);
+        diff.removeAll(intersect(hashSetA, hashSetB));
+        return diff;
+    }
 
 }
--- src/test/java/org/workout11/TestExercitii1.java
+++ src/test/java/org/workout11/TestExercitii1.java
@@ -3,8 +3,7 @@ package org.workout11;
 import org.junit.jupiter.api.Test;
 import org.workout11.ex1.Exercitii1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -51,6 +50,91 @@ public class TestExercitii1 {
         assertEquals(1, Exercitii1.occurences(2,arrayList));
         assertEquals(1, Exercitii1.occurences(5,arrayList));
         assertEquals(0, Exercitii1.occurences(8,arrayList));
+    }
+
+    @Test
+    void testMultiplyMatrixs() throws Exception {
+        Integer[][] A = { { 1, 1, 1 }
+                ,{ 2, 2, 2 }
+                ,{ 3, 3, 3 }
+                ,{ 4, 4, 4 } };
+
+        Integer[][] B = { { 1, 1, 1, 1 }
+                ,{ 2, 2, 2, 2 }
+                ,{ 3, 3, 3, 3 }};
+
+        Integer[][] X = { { 1, 1, 1, 1 }
+                ,{ 2, 2, 2, 2 }};
+
+        Integer[][] C ={ { 6, 6, 6, 6}
+                ,{ 12, 12, 12, 12 }
+                ,{ 18, 18, 18, 18 }
+                ,{ 24, 24, 24, 24}};
+
+
+        assertTrue(Arrays.deepEquals(Exercitii1.multiplyMatrix(A, B), C));
+        Exception exception= assertThrows(Exception.class,()->Exercitii1.multiplyMatrix(A,X));
+        assertEquals("matrixA[0].length must be equal with matrixB.length.", exception.getMessage());
+    }
+
+    @Test
+    void testIntersect(){
+        Set<Integer> setA = new HashSet<>();
+        setA.add(1);
+        setA.add(3);
+        setA.add(5);
+        setA.add(7);
+        Set<Integer> setB = new HashSet<>();
+        setB.add(3);
+        setB.add(6);
+        setB.add(7);
+        setB.add(8);
+        Set<Integer> setC = new HashSet<>();
+        setC.add(3);
+        setC.add(7);
+        assertEquals(setC,Exercitii1.intersect(setA,setB));
+    }
+
+    @Test
+    void testUnion(){
+        Set<Integer> setA = new HashSet<>();
+        setA.add(1);
+        setA.add(3);
+        setA.add(5);
+        setA.add(7);
+        Set<Integer> setB = new HashSet<>();
+        setB.add(3);
+        setB.add(6);
+        setB.add(7);
+        setB.add(8);
+        Set<Integer> setC = new HashSet<>();
+        setC.addAll(setA);
+        setC.addAll(setB);
+        assertEquals(setC, Exercitii1.union(setA,setB));
+    }
+
+    @Test
+    void testDifference(){
+        Set<Integer> setA = new HashSet<>();
+        setA.add(1);
+        setA.add(3);
+        setA.add(5);
+        setA.add(7);
+        Set<Integer> setB = new HashSet<>();
+        setB.add(3);
+        setB.add(6);
+        setB.add(7);
+        setB.add(8);
+        Set<Integer> setC = new HashSet<>();
+        setC.add(1);
+        setC.add(5);
+        Set<Integer> setD = new HashSet<>();
+        setD.add(6);
+        setD.add(8);
+        assertEquals(setC,Exercitii1.difference(setA,setB));
+        assertEquals(setD,Exercitii1.difference(setB,setA));
+
+        assertEquals(Exercitii1.union(setC,setD),Exercitii1.symmetricDifference(setB,setA));
     }
 
 }
